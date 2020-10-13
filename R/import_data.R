83fbe048ab283a33f8f920cfe35519f13de64794
kvittingseerup <k.vitting.seerup@gmail.com>
2020-10-13 14:25:03 +0200

    Fix bug introduced

--- R/import_data.R
+++ R/import_data.R
@@ -2840,14 +2840,9 @@ importIsoformExpression <- function(
     if(TRUE) {
         ### massage
         localDataList$abundance <- as.data.frame(localDataList$abundance)
-        if(countsFromAbundance) {
-            localDataList$counts <- as.data.frame(localDataList$countsFromAbundance)
-        } else {
-            localDataList$counts <- as.data.frame(localDataList$counts)
-        }
+        localDataList$counts <- as.data.frame(localDataList$counts)
         localDataList$length <- as.data.frame(localDataList$length)
-        localDataList$countsFromAbundance <- NULL
-
+        localDataList$countsFromAbundance <- NULL # remove message of how it was imported
 
         ### Fix names
         if( ignoreAfterBar | ignoreAfterSpace | ignoreAfterPeriod) {
